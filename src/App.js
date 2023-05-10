ec88f6959c1491a3807debc50d5963b78d9f862b
Tyler OBrien <tylerob65@gmail.com>
2023-05-09 23:14:28 -0400

    Added persisten login

--- src/App.js
+++ src/App.js
@@ -7,15 +7,26 @@ import SignUpPage from "./views/SignUpPage";
 import LoginPage from "./views/LoginPage";
 import MyCart from "./views/MyCart";
 
+const localLoginString = 'user_react_ecommerce'
+const getUserFromLocalStorage = () => {
+  const found = localStorage.getItem(localLoginString)
+  if (found) {
+    return JSON.parse(found)
+  }
+  return {}
+}
+
 export default function App() {
-  const [user,setUser] = useState({})
+  const [user,setUser] = useState(getUserFromLocalStorage)
 
   const logMeIn = (user) => {
     setUser(user)
+    localStorage.setItem(localLoginString,JSON.stringify(user))
   }
 
   const logMeOut = () => {
     setUser({})
+    localStorage.removeItem(localLoginString)
   }
 
   return (
